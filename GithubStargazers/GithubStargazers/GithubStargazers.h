da82e0075e923cd0f6058b61da3a71e6720cbed8
Stefano Martinallo <stefano.martinallo@urmet.com>
2022-06-14 23:14:09 +0200

    F Setup GithubStargazers framework
    Signed-off-by: Stefano Martinallo <stefano.martinallo@urmet.com>

---
+++ GithubStargazers/GithubStargazers/GithubStargazers.h
@@ -0,0 +1,18 @@
+//
+//  GithubStargazers.h
+//  GithubStargazers
+//
+//  Created by Stefano Martinallo on 14/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GithubStargazers.
+FOUNDATION_EXPORT double GithubStargazersVersionNumber;
+
+//! Project version string for GithubStargazers.
+FOUNDATION_EXPORT const unsigned char GithubStargazersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GithubStargazers/PublicHeader.h>
+
+
---
+++ GithubStargazers/GithubStargazersTests/GithubStargazersTests.swift
@@ -0,0 +1,16 @@
+//
+//  GithubStargazersTests.swift
+//  GithubStargazersTests
+//
+//  Created by Stefano Martinallo on 14/06/22.
+//
+
+import XCTest
+import GithubStargazers
+
+class GithubStargazersTests: XCTestCase {
+
+    func test_a() {
+        XCTFail("failing test")
+    }
+}
