314a39692a278110dbb181e8e89f0eb18b880c08
Stefano Martinallo <stefano.martinallo@urmet.com>
2022-06-14 23:42:13 +0200

    F Ensure loadStargazers hits github url
    Signed-off-by: Stefano Martinallo <stefano.martinallo@urmet.com>

--- GithubStargazers/GithubStargazersTests/GithubService_Tests.swift
+++ GithubStargazers/GithubStargazersTests/GithubService_Tests.swift
@@ -7,7 +7,9 @@
 
 import XCTest
 
-protocol HTTPClient {}
+protocol HTTPClient {
+    func get(url: URL)
+}
 
 final class GithubService {
     private let client: HTTPClient
@@ -15,10 +17,20 @@ final class GithubService {
     init(_ client: HTTPClient) {
         self.client = client
     }
+
+    func loadStargazers(forUser user: String, withRepo repo: String) {
+        let url = URL(string: "https://api.github.com/repos/\(user)/\(repo)/stargazers")
+
+        client.get(url: url!)
+    }
 }
 
 class HTTPClientSpy: HTTPClient {
-    private(set) var requestedUrls = [String]()
+    private(set) var requestedUrls = [URL]()
+
+    func get(url: URL) {
+        requestedUrls.append(url)
+    }
 }
 
 class GithubService_Tests: XCTestCase {
@@ -27,6 +39,26 @@ class GithubService_Tests: XCTestCase {
         let (client, _) = makeSUT()
 
         XCTAssertEqual([], client.requestedUrls, "expecting sut not to perform any requests upon creation")
+    }
+
+    func test_loadStargazers_requestCorrectUrlEveryTimeIsInvoked() {
+        let user = "a-user"
+        let repo = "a-repo"
+        let otherUser = "other-user"
+        let otherRepo = "other-repo"
+        let (client, sut) = makeSUT()
+
+        sut.loadStargazers(forUser: user, withRepo: repo)
+        sut.loadStargazers(forUser: otherUser, withRepo: otherRepo)
+
+        XCTAssertEqual(
+            [
+                URL(string: "https://api.github.com/repos/\(user)/\(repo)/stargazers"),
+                URL(string: "https://api.github.com/repos/\(otherUser)/\(otherRepo)/stargazers")
+            ],
+            client.requestedUrls,
+            "expecting sut to hit github endpoint every time loadStargazers is invoked"
+        )
     }
 
     // MARK: - Helpers
