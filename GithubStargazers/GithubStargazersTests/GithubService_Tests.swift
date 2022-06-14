2cdf6e68c875df18d9c18327c14ab18fe0c3cae6
Stefano Martinallo <stefano.martinallo@urmet.com>
2022-06-14 23:21:15 +0200

    F Ensure GithubService does not perform requests upon creation
    Signed-off-by: Stefano Martinallo <stefano.martinallo@urmet.com>

--- GithubStargazers/GithubStargazersTests/GithubService_Tests.swift
+++ GithubStargazers/GithubStargazersTests/GithubService_Tests.swift
@@ -7,29 +7,25 @@
 
 import XCTest
 
+final class GithubService {
+    private let client: HTTPClientSpy
+
+    init(_ client: HTTPClientSpy) {
+        self.client = client
+    }
+}
+
+class HTTPClientSpy {
+    private(set) var requestedUrls = [URL]()
+}
+
 class GithubService_Tests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func test_load_doesNotRequestUrlUponCreation() {
+        let client = HTTPClientSpy()
+        let _ = GithubService(client)
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssertEqual([], client.requestedUrls, "expecting sut not to perform any requests upon creation")
     }
 
 }
