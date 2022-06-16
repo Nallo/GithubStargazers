aa2e786cfaf6f7de26d34ba1623d0fdc17f21465
Stefano Martinallo <stefano.martinallo@urmet.com>
2022-06-16 17:19:44 +0200

    F Add URLSessionHTTPClient implementation

--- GithubStargazers/GithubStargazers/HTTPClient.swift
+++ GithubStargazers/GithubStargazers/HTTPClient.swift
@@ -13,3 +13,26 @@ public protocol HTTPClient {
 
     func get(url: URL, headers: HTTPHeader, completion: @escaping (Result) -> Void)
 }
+
+public final class URLSessionHTTPClient: HTTPClient {
+
+    private let session: URLSession
+
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+
+    public func get(url: URL, headers: HTTPHeader, completion: @escaping (HTTPClient.Result) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue(headers.headerValue, forHTTPHeaderField: headers.headerField)
+
+        session.dataTask(with: request) { data, response, error in
+            if let data = data, let httpResponse = response as? HTTPURLResponse {
+                completion(.success((data, httpResponse)))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
