4e7d578a44cfbcee33f1dbebd00f4671cece0a0d
Stefano Martinallo <stefano.martinallo@urmet.com>
2022-06-16 22:12:23 +0200

    F Add `GithubStargazersiOS` target (prod and test)

---
+++ GithubStargazers/GithubStargazersiOS/GithubStargazersiOS.h
@@ -0,0 +1,18 @@
+//
+//  GithubStargazersiOS.h
+//  GithubStargazersiOS
+//
+//  Created by Stefano Martinallo on 16/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GithubStargazersiOS.
+FOUNDATION_EXPORT double GithubStargazersiOSVersionNumber;
+
+//! Project version string for GithubStargazersiOS.
+FOUNDATION_EXPORT const unsigned char GithubStargazersiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GithubStargazersiOS/PublicHeader.h>
+
+
