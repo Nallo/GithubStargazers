da4d82b7d6ee5c8a3a32fd99d42d0940fc1242aa
Stefano Martinallo <stefano.martinallo@urmet.com>
2022-06-17 15:54:03 +0200

    F Ensure stargazers are loaded on viewDidLoad invocation

--- GithubStargazers/GithubStargazersiOSTests/StargazersViewController_Tests.swift
+++ GithubStargazers/GithubStargazersiOSTests/StargazersViewController_Tests.swift
@@ -8,19 +8,48 @@
 import XCTest
 import GithubStargazers
 
-final class StargazersViewController {
+final class StargazersViewController: UIViewController {
 
-    init(service: StargazersLoader) {}
+    private var loader: StargazersLoader?
+    private var user: String?
+    private var repository: String?
+
+    convenience init(loader: StargazersLoader, user: String, repository: String) {
+        self.init()
+        self.loader = loader
+        self.user = user
+        self.repository = repository
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loader?.loadStargazers(forUser: user!, withRepo: repository!) { _ in }
+
+    }
 
 }
 
 class StargazersViewController_Tests: XCTestCase {
 
     func test_init_doesNotTriggerLoad() {
+        let user = "any-user"
+        let repository = "any-repository"
         let service = StargazersLoaderSpy()
-        let _ = StargazersViewController(service: service)
+        let _ = StargazersViewController(loader: service, user: user, repository: repository)
 
         XCTAssertEqual(0, service.loadCallCount)
+    }
+
+    func test_viewDidLoad_triggersStargazersLoad() {
+        let user = "any-user"
+        let repository = "any-repository"
+        let service = StargazersLoaderSpy()
+        let sut = StargazersViewController(loader: service, user: user, repository: repository)
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(1, service.loadCallCount)
     }
 
     // MARK: - Helpers
@@ -32,6 +61,7 @@ class StargazersLoaderSpy: StargazersLoader {
     private(set) var loadCallCount = 0
 
     func loadStargazers(forUser user: String, withRepo repo: String, completion: @escaping Completion) {
+        loadCallCount += 1
     }
 
 }
