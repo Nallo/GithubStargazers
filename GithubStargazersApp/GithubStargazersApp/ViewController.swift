4a3d22325d9cb9e1da2593ee62c951ad9334c58f
Stefano Martinallo <stefano.martinallo@urmet.com>
2022-06-18 15:27:03 +0200

    F Add template iOS app

---
+++ GithubStargazersApp/GithubStargazersApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GithubStargazersApp
+//
+//  Created by Stefano Martinallo on 18/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+}
+
